ad4dfdc4c085144f24207fa3e13a79c643651904
hamzahalhalabi1 <80872291+hamzahalhalabi1@users.noreply.github.com>
2024-03-20 00:00:48 +0300

    Delete week 2 directory

--- week 2/camel/camel.py
+++
@@ -1,8 +0,0 @@
-name = input('camelCase: check50 cs50/problems/2022/python/camel')
-
-for c in name:
-    if c.isupper():
-        character = c
-        name = name.replace(c,f'_{character.lower()}' )
-
-print(name)

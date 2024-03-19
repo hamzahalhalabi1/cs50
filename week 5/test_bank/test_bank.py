49d3931b72b4a02978aaf3c68dd4186bc18f256c
hamzahalhalabi1 <80872291+hamzahalhalabi1@users.noreply.github.com>
2024-03-20 00:01:18 +0300

    Delete week 5 directory

--- week 5/test_bank/test_bank.py
+++
@@ -1,9 +0,0 @@
-from bank import value
-import pytest
-
-
-def test_value():
-    assert value('hello') == 0
-    assert value('hfdsbn') == 20
-    assert value('fjkdfjdkf') == 100
-    assert value('HELLO') == 0
